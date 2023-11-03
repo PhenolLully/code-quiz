b1c09384c6f0472d4143434bd184375e9f554755
Phenol Lully <lullyp95@gmail.com>
2023-11-03 17:59:21 -0400

    Refactored a lot of Js code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,53 +1,89 @@
 var timer = document.querySelector("#timer");
 var count = 60;
 var question = document.querySelector("#question")
+var answers = document.querySelector("#answers")
 var currentQuestion = 0;
+var startQuiz = document.querySelector("#start-quiz");
+
 var holdQuestion = [
 {
     question: "Which one is a tag selector?", 
     answers: ["?", "$", ".", "<p>"],
-    answerKey: "<p>"
+    correctAnswer: "<p>"
 
 },
 {
-    question: "Which one is a tag selector?", 
-    answers: ["?", "$", ".", "<p>"],
-    answerKey: "<p>"
+    question: "What does CSS stand for?", 
+    answers: ["Computer Style Sheets", "Creative Style Sheets", "Cascading Style Sheets", "Colorful Style Sheets"],
+    correctAnswer: "Cascading Style Sheets"
     
 },
 {
-    question: "Which one is a tag selector?", 
-    answers: ["?", "$", ".", "<p>"],
-    answerKey: "<p>"
+    question: "Which CSS property is used to control the size of an element's content area?", 
+    answers: ["width", "size", "content-size", "box-size"],
+    correctAnswer: "width"
     
 },
 {
-    question: "Which one is a tag selector?", 
-    answers: ["?", "$", ".", "<p>"],
-    answerKey: "<p>"
+    question: "What is the purpose of the addEventListener method in JavaScript?", 
+    answers: ["To add an event handler function to an element", "To create a new HTML element", "To change the page's title", "To modify the styling of an element"],
+    correctAnswer: "To add an event handler function to an element"
     
 }
-]           
+];
+
+startQuiz.addEventListener("click", function(){
+    alert("The quiz has started!")
+    startGame();
+    myTimer();
+});
+
 
 function myTimer(){
   var timeInterval = setInterval(function(){
     count--;
-    timer.textContent = count;
+    timer.textContent = "Time: " + count;
     if(count <= 0){
         clearInterval(timeInterval);
     }
-  },1000);
-
+}, 1000);
 }
-
-myTimer();
-
 
 function startGame(){
-    question.textContent = holdQuestion[0].question;
+    displayQuestion(currentQuestion);
 }
 
+function displayQuestion(questionIndex){
+    question.textContent = holdQuestion[questionIndex].question;
+    answers.innerHTML = "";
 
-function nextQuestion(){
-    currentQuestion++
-}
+    for (var i = 0; i < holdQuestion[questionIndex].answers.length; i++) {
+        var answerItem = document.createElement("li");
+        answerItem.textContent = holdQuestion[questionIndex].answers[i];
+        answerItem.addEventListener("click", function () {
+          // Handle user's answer selection
+          var selectedAnswer = this.textContent;
+          var correctAnswer = holdQuestion[questionIndex].correctAnswer;
+          if (selectedAnswer === correctAnswer) {
+            feedback.textContent = "Correct!";
+            // Handle correct answer
+          } else {
+            // Handle incorrect answer
+            feedback.textContent = "Wrong!";
+          }
+
+          // Move to the next question
+          currentQuestion++;
+          if (currentQuestion < holdQuestion.length) {
+            displayQuestion(currentQuestion);
+          } else {
+            feedback.textContent = "Quiz Is Over"
+            // End the quiz as there are no more questions
+          }
+        });
+
+        answers.appendChild(answerItem);
+      }
+    }
+
+    
